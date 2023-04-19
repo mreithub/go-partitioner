f37888cb2c3ad17c1ce682d5c7c10ba192dd58ab
Manuel Reithuber <manuel@reithuber.net>
2023-04-19 14:35:26 +0200

    pgxdriver: using value instead of pointer receivers

--- driver/pgxdriver/driver.go
+++ driver/pgxdriver/driver.go
@@ -15,7 +15,7 @@ type PGXDriver struct {
 	Context context.Context
 }
 
-func (d *PGXDriver) CreatePartition(info driver.CreatePartitionInfo) error {
+func (d PGXDriver) CreatePartition(info driver.CreatePartitionInfo) error {
 	// pgx (or postgres itself) doesn't seem to support parameters for this query -> we're using unescaped time strings here
 	// TODO find a way to properly sanitize input (this method should only be called internally with safe parameters but still...)
 	var sql = fmt.Sprintf("CREATE TABLE %s PARTITION OF %s FOR VALUES FROM ('%s') TO ('%s')",
@@ -24,12 +24,12 @@ func (d *PGXDriver) CreatePartition(info driver.CreatePartitionInfo) error {
 	return err
 }
 
-func (d *PGXDriver) DropPartition(name string) error {
+func (d PGXDriver) DropPartition(name string) error {
 	var _, err = d.DB.Exec(d.Context, fmt.Sprintf("DROP TABLE %s", pgx.Identifier{name}.Sanitize()))
 	return err
 }
 
-func (d *PGXDriver) ListExistingPartitions(table string) (map[string]bool, error) {
+func (d PGXDriver) ListExistingPartitions(table string) (map[string]bool, error) {
 	// fetch existing partitions:
 	var rows, err = d.DB.Query(d.Context, "SELECT inhrelid::regclass FROM pg_catalog.pg_inherits WHERE inhparent = $1::regclass", table)
 	defer rows.Close()
